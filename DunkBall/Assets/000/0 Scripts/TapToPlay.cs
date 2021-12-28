7876d0b3767060212479973311910ac670bde353
caglar <caglarkutluyigit@gmail.com>
2021-12-29 01:25:12 +0300

    Update TapToPlay.cs

--- DunkBall/Assets/000/0 Scripts/TapToPlay.cs
+++ DunkBall/Assets/000/0 Scripts/TapToPlay.cs
@@ -5,8 +5,14 @@ using DG.Tweening;
 
 public class TapToPlay : MonoBehaviour
 {
+    Sequence sequence;
     void Start()
     {
-        transform.DOScale(Vector3.one * 1.1f, 1).SetLoops(-1, LoopType.Yoyo);
+        sequence = DOTween.Sequence();
+        sequence.Append(transform.DOScale(Vector3.one * 1.1f, 1).SetLoops(-1, LoopType.Yoyo));
+    }
+    private void OnDisable()
+    {
+        sequence.Kill();
     }
 }
